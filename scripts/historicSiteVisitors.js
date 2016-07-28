92fc18961ebb8d4728b3faa6b9ea4c5b5a822597
Evan Porter <evan.porter@dotman.ca>
2016-07-28 19:31:16 -0300

    Only use 1 JavaScript file

--- scripts/historicSiteVisitors.js
+++ scripts/historicSiteVisitors.js
@@ -69,3 +69,33 @@ HISTORIC_DATA = (function () {
         }
     };
 }());
+
+(function () {
+    HISTORIC_DATA.loadData(function () {
+        // Success! Load the chart!
+        var container = document.getElementById('chart');
+        var locations = HISTORIC_DATA.getUniquePlaces();
+        var items = HISTORIC_DATA.getAllData();
+
+        var groups = new vis.DataSet();
+        locations.forEach(function (location, index) {
+            groups.add({
+                id: index,
+                content: location
+            });
+        });
+
+        var dataSet = new vis.DataSet(items);
+
+        var options = {
+            drawPoints: true,
+            dataAxis: {visible: false},
+            legend: true
+        };
+
+        var graph2d = new vis.Graph2d(container, dataSet, groups, options);
+    }, function () {
+        // Failure. WTF.
+        document.getElementById("chart").innerHTML = "Error loading data.";
+    });
+}());
