a4718c6c40b0e809e2ef7f5897dee2e08b2a7007
Evan Porter <evan.porter@dotman.ca>
2016-08-13 19:09:57 -0300

    Refactoring a bit to remove hisotric data manipulation from data loading

--- scripts/historicSiteVisitors.js
+++ scripts/historicSiteVisitors.js
@@ -4,27 +4,16 @@
 HISTORIC_DATA = (function () {
     "use strict";
 
-    var historicData = null;
-
-    // Months are numbers, so we need to pad them with zeros
-    var normalizeMonths = function (dataArray) {
-        dataArray.forEach(function (item) {
-            if (item["Month"] && item["Month"].length === 1) {
-                item["Month"] = "0" + item["Month"];
-            }
-        });
-
-        return dataArray;
-    };
+    var data = null;
 
     return {
-        loadData: function (successCallback, errorCallback) {
+        loadData: function (dataUrl, successCallback, errorCallback, transformData) {
             var xhr = new XMLHttpRequest();
 
             xhr.onload = function () {
                 if (this.status == 200 && this.responseText) {
                     // success!
-                    historicData = normalizeMonths(JSON.parse(this.responseText));
+                    data = transformData(JSON.parse(this.responseText));
                     successCallback();
                 } else {
                     // something went wrong
@@ -32,20 +21,20 @@ HISTORIC_DATA = (function () {
                 }
             };
 
-            xhr.open("get", "data/historic_site_visitors.json", true);
+            xhr.open("get", dataUrl, true);
             xhr.send(null);
         },
         getUniquePlaces: function () {
             var placeMap = {};
 
-            historicData.forEach(function (item) {
+            data.forEach(function (item) {
                 placeMap[item["Location"]] = true;
             });
 
             return Object.keys(placeMap).sort();
         },
         getDataForLocation: function (location) {
-            var placeData = historicData.filter(function (item) {
+            var placeData = data.filter(function (item) {
                 return item["Location"] === location;
             });
 
@@ -59,7 +48,7 @@ HISTORIC_DATA = (function () {
         getAllData: function () {
             var locations = this.getUniquePlaces();
 
-            return historicData.map(function (item) {
+            return data.map(function (item) {
                 return {
                     x: item["Year"] + "-" + item["Month"],
                     y: item["Visitors"],
@@ -71,7 +60,7 @@ HISTORIC_DATA = (function () {
 }());
 
 (function () {
-    HISTORIC_DATA.loadData(function () {
+    HISTORIC_DATA.loadData("data/historic_site_visitors.json", function () {
         "use strict";
 
         // Success! Load the chart!
@@ -99,5 +88,13 @@ HISTORIC_DATA = (function () {
     }, function () {
         // Failure. WTF.
         document.getElementById("chart").innerHTML = "Error loading data.";
+    }, function (dataArray) {
+        dataArray.forEach(function (item) {
+            if (item["Month"] && item["Month"].length === 1) {
+                item["Month"] = "0" + item["Month"];
+            }
+        });
+
+        return dataArray;
     });
 }());
